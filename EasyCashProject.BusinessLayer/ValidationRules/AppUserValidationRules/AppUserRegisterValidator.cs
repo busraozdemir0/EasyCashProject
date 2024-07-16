a80834c18dd16fd613068bb46f002e996767096d
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-07-17 00:40:21 +0300

    AppUserRegister için Fluent Validation

---
+++ EasyCashProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,57 @@
+﻿using EasyCashProject.DtoLayer.Dtos.AppUserDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashProject.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Ad alanı boş geçilemez.");
+
+            RuleFor(x => x.Surname)
+                .NotEmpty()
+                .WithMessage("Soyad alanı boş geçilemez.");
+
+            RuleFor(x => x.UserName)
+                .NotEmpty()
+                .WithMessage("Kullanıcı adı alanı boş geçilemez.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("E-Mail alanı boş geçilemez.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Şifre alanı boş geçilemez.");
+
+            RuleFor(x => x.ConfirmPassword)
+                .NotEmpty()
+                .WithMessage("Şifre tekrar alanı boş geçilemez.");
+
+            RuleFor(x => x.Name)
+                .MaximumLength(30)
+                .WithMessage("Lütfen en fazla 30 karakter girişi yapın.");
+
+            RuleFor(x => x.Name)
+                .MinimumLength(30)
+                .WithMessage("Lütfen en az 2 karakter girişi yapın.");
+
+            RuleFor(x => x.Email)
+                .EmailAddress()
+                .WithMessage("Lütfen geçerli bir mail adresi giriniz.");
+
+            // Sifrelerin eslesip eslesmedigi kontrolu
+            RuleFor(x => x.ConfirmPassword)
+                .Equal(y => y.Password)
+                .WithMessage("Parolalarınız eşleşmiyor!");
+        }
+    }
+}
