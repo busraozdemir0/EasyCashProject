cff4d30e7f9e07ca20672375a19765c53ff70e07
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-07-17 14:41:48 +0300

    interface ve repository'lerin yazılması & tema dosyaları

---
+++ EasyCashProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using EasyCashProject.DataAccessLayer.Abstract;
+using EasyCashProject.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashProject.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(T t)
+        {
+            using var context = new Context();
+            context.Set<T>().Remove(t);
+            context.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            using var context = new Context();
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            using var context = new Context();
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            using var context = new Context();
+            context.Set<T>().Add(t);
+            context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            using var context = new Context();
+            context.Set<T>().Update(t);
+            context.SaveChanges();
+        }
+    }
+}
